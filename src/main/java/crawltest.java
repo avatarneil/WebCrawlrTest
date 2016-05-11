2faa042bb4f7dd9adcf59aa9afc48c9313cd837d
Avatarneil <avatarneil@gmail.com>
2016-05-11 14:59:09 -0400

    Added new site, game plan is going to be to search page for hyperlinks with "csv" in the title, as well as csv in plaintext on the page, and return those links.

--- src/main/java/crawltest.java
+++ src/main/java/crawltest.java
@@ -12,19 +12,19 @@ public class crawltest {
     public static void main(String[] args) {
         try {
             // fetch the document over HTTP
-            Document doc = Jsoup.connect("randomsite.org").get();
+            Document doc = Jsoup.connect("https://offshoreleaks.icij.org/pages/database").get();
 
             // get the page title
-            //String title = doc.title();
-            //System.out.println("title: " + title);
+            String title = doc.title();
+            System.out.println("title: " + title);
 
             // get all links in page
-            /*Elements links = doc.select("a[href]");
+            Elements links = doc.select("a[href]");
             for (Element link : links) {
                 // get the value from the href attribute
                 System.out.println("\nlink: " + link.attr("href"));
                 System.out.println("text: " + link.text());
-            }*/
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
