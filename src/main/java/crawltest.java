88a52acc1d7f76e06348446ee270e6b1bf9e46ae
Avatarneil <avatarneil@gmail.com>
2016-05-11 14:55:33 -0400

    Added try and catch, currently connecting to "https://flatearthsociety.org", but will be updating that soon to a site with meaningful data.

--- src/main/java/crawltest.java
+++ src/main/java/crawltest.java
@@ -10,24 +10,25 @@ import org.jsoup.select.Elements;
 
 public class crawltest {
     public static void main(String[] args) {
-       /* try {
+        try {
             // fetch the document over HTTP
-            Document doc = Jsoup.connect("http://flatearthsociety.org").get();
+            Document doc = Jsoup.connect("randomsite.org").get();
 
             // get the page title
-            String title = doc.title();
-            System.out.println("title: " + title);
+            //String title = doc.title();
+            //System.out.println("title: " + title);
 
             // get all links in page
-            Elements links = doc.select("a[href]");
+            /*Elements links = doc.select("a[href]");
             for (Element link : links) {
                 // get the value from the href attribute
                 System.out.println("\nlink: " + link.attr("href"));
                 System.out.println("text: " + link.text());
-            }
+            }*/
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 }
+
 
