e4fb81f07352a6b6a837c9d7fa13b262819c6399
Avatarneil <avatarneil@gmail.com>
2016-05-13 11:12:03 -0400

    added some more parsing stuff, but am having issues using getHeaderMap().

--- src/main/java/sortCSV.java
+++ src/main/java/sortCSV.java
@@ -12,17 +12,20 @@ import java.io.File;
 import java.nio.file.Paths;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 
 public class sortCSV {
-    public static void makeArray() throws IOException {
+    public static void main(String[] args) throws IOException {
         try {
             Reader in = new FileReader("data/GeoIPv6.csv");
-            Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
-            for (CSVRecord record : records) {
-                String lastName = record.get("Last Name");
-                String firstName = record.get("First Name");
-                System.out.println(lastName + firstName);
+            CSVParser parser = new CSVParser(in, CSVFormat.EXCEL);
+            //System.out.println(parser.getHeaderMap().size());
+            List<CSVRecord> list = parser.getRecords();
+            System.out.println(list.size());
+            for (CSVRecord record : list) {
+                String lastName = record.get(0);
+                //System.out.println(lastName + firstName);
             }
         } catch (IOException e) {
             e.printStackTrace();
